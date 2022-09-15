74a1ef21f235b3f4ed718488aeca113e99b0992e
asm-dev <salamanca.m@outlook.com>
2022-09-15 16:16:52 +0200

    Day 8

--- 007-Caesar-cipher.py
+++ 007-Caesar-cipher.py
@@ -14,6 +14,8 @@ def pos_letra (letra, lista):
 abecedario = list("abcdefghijklmnñopqrstuvwxyz")
 
 def encriptador_cesar (mensaje, num, encriptar):
+    print(mensaje)
+    print(num)  
     nuevo_mensaje = []
     if encriptar == "D":
         num = -num 
@@ -21,12 +23,16 @@ def encriptador_cesar (mensaje, num, encriptar):
     for letra in lista_mensaje:
         posicion_letra = pos_letra(letra, abecedario) #cada letra del abecedario, compara letra que damos con ella, si coincide, nos devuelve la posicion de esa letra en el abecedario
         nueva_posicion_letra = int(posicion_letra[0]) + num
-        nueva_letra = abecedario[nueva_posicion_letra]
-        print(f"Letra: {letra}, Posición: {posicion_letra}, Nueva posicion: {nueva_posicion_letra}, Nueva letra: {nueva_letra}") #posicion de la letra en el abecedario
-        #!!!! Problemita con la A (-5) -- ninguno aparentemente
-        # y la Z? -- list index out of range (len list...)
-    print(mensaje)
-    print(num)
+        if nueva_posicion_letra > len(abecedario):
+            nueva_posicion_letra = nueva_posicion_letra - len(abecedario)
+            nueva_letra = abecedario[nueva_posicion_letra]
+            print(f"Letra: {letra}, Posición: {posicion_letra}, Nueva posicion: {nueva_posicion_letra}, Nueva letra:{nueva_letra}") #tiene que ser e
+            print(f"Longitud abd: {len(abecedario)}")
+        else:
+            nueva_letra = abecedario[nueva_posicion_letra]
+            print(f"Letra: {letra}, Posición: {posicion_letra}, Nueva posicion: {nueva_posicion_letra}, Nueva letra: {nueva_letra}") #posicion de la letra en el abecedario
+        #-25???
+    
     return ""
 print(encriptador_cesar("holaz", 5, ""))
 print(encriptador_cesar("holaz", 5, "D"))
