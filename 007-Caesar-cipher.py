fa9f5c4afa0a02cf4f2f3a9b4c1dc74cc233d768
asm-dev <salamanca.m@outlook.com>
2022-09-15 17:17:16 +0200

    Day 8

--- 007-Caesar-cipher.py
+++ 007-Caesar-cipher.py
@@ -1,42 +1,24 @@
-# encriptar = input("Escribe D para desencriptar, E para encriptar:\n")
-# #Mensaje, sin espacios
-# mensaje = input("Escribe tu mensaje (sin espacios): ").lower()
-# #Pedir num
-# num = input("Número: ")
+from functions import pos_letra  
+from functions import encriptador_cesar
 
-def pos_letra (letra, lista):
-    posiciones = []
-    for i in lista:
-        if letra == i:
-            posiciones.append(abecedario.index(i))
-    return posiciones
-    
-abecedario = list("abcdefghijklmnñopqrstuvwxyz")
+print(f"\nBienvenido al encriptador César\n")
 
-def encriptador_cesar (mensaje, num, encriptar):
-    print(mensaje)
-    print(num)  
-    nuevo_mensaje = []
-    if encriptar == "D":
-        num = -num 
-    lista_mensaje = list(mensaje)
-    for letra in lista_mensaje:
-        posicion_letra = pos_letra(letra, abecedario) #cada letra del abecedario, compara letra que damos con ella, si coincide, nos devuelve la posicion de esa letra en el abecedario
-        nueva_posicion_letra = int(posicion_letra[0]) + num
-        if nueva_posicion_letra > len(abecedario):
-            nueva_posicion_letra = nueva_posicion_letra - len(abecedario)
-            nueva_letra = abecedario[nueva_posicion_letra]
-            print(f"Letra: {letra}, Posición: {posicion_letra}, Nueva posicion: {nueva_posicion_letra}, Nueva letra:{nueva_letra}") #tiene que ser e
-            print(f"Longitud abd: {len(abecedario)}")
-        else:
-            nueva_letra = abecedario[nueva_posicion_letra]
-            print(f"Letra: {letra}, Posición: {posicion_letra}, Nueva posicion: {nueva_posicion_letra}, Nueva letra: {nueva_letra}") #posicion de la letra en el abecedario
-        #-25???
-    
-    return ""
-print(encriptador_cesar("holaz", 5, ""))
-print(encriptador_cesar("holaz", 5, "D"))
+de_nuevo = True
+while de_nuevo == True:
 
-# nuevo_mensaje = encriptador_cesar(mensaje, num, encriptar)
+    mensaje = input("\nEscribe tu mensaje (sin espacios):\n").lower()
+    encriptar = input("\nEscribe D para desencriptar, E para encriptar:\n").upper()
+    valor = int(input("\nNúmero:\n"))
+    abecedario = list("abcdefghijklmnñopqrstuvwxyz")
 
-# Dar la opcion de hacerlo otra vez
+    #TESTEO:
+    # hola_enc = encriptador_cesar("hola", 5, "E", abecedario)
+    # hola_des = encriptador_cesar(hola_enc, 5, "D", abecedario)
+    # print(hola_enc)
+    # print(hola_des)
+
+    nuevo_mensaje = encriptador_cesar(mensaje, encriptar, valor, abecedario)
+    print(f"Tu nuevo mensaje es {nuevo_mensaje}")
+
+    if input("¿Quieres encriptar o desencriptar otro mensaje? (S/N) ") != "S":
+        de_nuevo = False
--- functions.py
+++ functions.py
@@ -11,3 +11,62 @@ def comprueba_letra (palabra, letra):
         if palabra[n] == letra:
             posicion.append(n)
     return posicion
+
+
+#Función empleada en el proyecto 007-Caesar-cipher, dentro de la funcion encriptador_cesar
+def pos_letra (letra, lista):
+    """
+    Esta función compara una letra con los elementos de una lista.
+    Se itera la lista y se compara cada uno de los elementos de la lista con la letra.
+    Si algún elemento de la lista es igual a la letra, se añade a la lista vacía posiciones el index en la lista de ese elemento.
+    Esta función se emplea dentro de la función César para conseguir el idx en el abecedario de una letra.
+    """
+    posiciones = []
+    for i in lista:
+        if letra == i:
+            posiciones.append(lista.index(i))
+    return posiciones #Nos devuelve una lista, podemos conseguir el item con posiciones[0]
+
+
+#Función empleada en el proyecto 007-Caesar-cipher
+def encriptador_cesar (mensaje, encriptar, num, abc):
+    """
+    Esta funcion nos permite cifrar y descifrar un mensaje. Es un encriptador.
+    Los param son el mensaje, el número de saltos en la lista, la dirección de los saltos (+/D para encriptar, -/IZQ para desencriptar) y una lista de valores (generalmente un abecedario)
+    El funcionamiento interno está explicado con comentarios y también hay un par de prints comentados que pueden usarse para entender el funcionamiento de esta función.
+    Internamente utiliza la función pos_letra.
+    """
+    if encriptar == "D":
+        num = -num 
+        print(f"\nTu mensaje es {mensaje}, y lo vamos a desencriptar, usando un valor de {num}\n")
+    elif encriptar == "E":
+        print(f"\nTu mensaje es {mensaje}, y lo vamos a encriptar, usando un valor de {num}\n")
+    else:
+        print("\nEncriptar debe de ser D o E, la función no admite otras opciones para este parámetro\n")
+
+    lista_mensaje = list(mensaje) #Pasamos de string a list
+    nuevo_mensaje_lista = [] #Creamos una lista vacía para el nuevo mensaje
+
+    for letra in lista_mensaje: #Iteramos sobre el mensaje (lista compuesta por cada una de las letras del mensaje a encriptar)
+
+        posicion_letra = pos_letra(letra, abc) #Comparamos cada letra del mensaje con las letras del abecedario, cuando hay coincidencia, nos devuelve el index en el abcedario
+        nueva_posicion_letra = posicion_letra[0] + num #Añade el valor al index de la letra del mensaje
+
+        if nueva_posicion_letra > len(abc): #Puede ser que la nueva posición sea mayor que la longitud del abecedario, por ejemplo, index de Z + 5
+            nueva_posicion_letra = nueva_posicion_letra - len(abc)
+            nueva_letra = abc[nueva_posicion_letra]
+
+            # PRINTS PARA ENTENDER QUÉ PASA AQUÍ:
+            # print(f"Letra: {letra}, Posición: {posicion_letra}, Nueva posicion: {nueva_posicion_letra}, Nueva letra:{nueva_letra}") #tiene que ser e
+            # print(f"Longitud abd: {len(abecedario)}")
+
+        else:
+            nueva_letra = abc[nueva_posicion_letra]
+            # PRINT PARA ENTENDER QUÉ PASA AQUÍ:
+            # print(f"Letra: {letra}, Posición: {posicion_letra}, Nueva posicion: {nueva_posicion_letra}, Nueva letra: {nueva_letra}") #posicion de la letra en el abecedario
+        
+        nuevo_mensaje_lista.append(nueva_letra)
+        nuevo_mensaje = ''.join(nuevo_mensaje_lista)
+
+    return nuevo_mensaje
+        
