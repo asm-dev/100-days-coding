0bd09456a71ce13f659168b1d31022fedb7757cd
asm-dev <salamanca.m@outlook.com>
2022-09-19 18:13:17 +0200

    Day 11

--- 007-Caesar-cipher.py
+++ 007-Caesar-cipher.py
@@ -1,7 +1,14 @@
 from functions import pos_letra  
 from functions import encriptador_cesar
 
+def evalua (str, abc):
+        for letra in str:
+            if not letra in abc:
+                return False
+
 print(f"\nBienvenido al encriptador César\n")
+
+abecedario = list("abcdefghijklmnñopqrstuvwxyz")
 
 de_nuevo = True
 while de_nuevo == True:
@@ -9,18 +16,21 @@ while de_nuevo == True:
     mensaje = input("\nEscribe tu mensaje (sin espacios):\n").lower()
     encriptar = input("\nEscribe D para desencriptar, E para encriptar:\n").upper()
     valor = int(input("\nNúmero:\n"))
-    abecedario = list("abcdefghijklmnñopqrstuvwxyz")
-
+    
+    while evalua (mensaje, abecedario) == False:
+        print(f"\nAl menos uno de los caracteres introducidos no se encuentra en el abecedario. Puedes introducir mensajes que contengan los siguientes caracteres {abecedario}.\n\nPor favor introduce un mensaje válido.\n")
+        mensaje = input("\nEscribe tu mensaje (sin espacios):\n").lower()
+        encriptar = input("\nEscribe D para desencriptar, E para encriptar:\n").upper()
+        valor = int(input("\nNúmero:\n"))
+    
+    nuevo_mensaje = encriptador_cesar(mensaje, encriptar, valor, abecedario)
+    print(f"Tu nuevo mensaje es {nuevo_mensaje}")
+        
     # TESTEO:
     # hola_enc = encriptador_cesar("hola", 5, "E", abecedario)
     # hola_des = encriptador_cesar(hola_enc, 5, "D", abecedario)
     # print(hola_enc)
     # print(hola_des)
 
-    nuevo_mensaje = encriptador_cesar(mensaje, encriptar, valor, abecedario)
-    print(f"Tu nuevo mensaje es {nuevo_mensaje}")
-
-    if input("¿Quieres encriptar o desencriptar otro mensaje? (S/N) ").upper() != "S":
+    if input("\n¿Quieres encriptar o desencriptar otro mensaje? (S/N) ").upper() != "S":
         de_nuevo = False
-
-## A solucionar/mejorar -- error con caracteres que no esten en el abc como "#" usando la lista abecedario
