3c55ee6d718d7a4e27c67011c54ee894e5baf982
asm-dev <salamanca.m@outlook.com>
2022-09-23 18:55:37 +0200

    Day 14

--- 009-Calculator-OOP.py
+++ 009-Calculator-OOP.py
@@ -1,4 +1,74 @@
-# Un poco de mezcla entre programación funcional y programación orientada a objetos, para ir probando la POO en Python...
+# Aproximación a la POO... Bastantes cosas aún por mejorar!
+
+# POR HACER
+# *********
+# Las operaciones hacen un print, cambiar por return, así luego con el resultado devuelto podemos imprimir o hacer lo que queramos
+# Además dejamos la funcionalidad fuera de la clase
+
+# Mi clase actual:
+# +------------------------+
+# | Calculadora            |
+# +------------------------+
+# | num1: float            |
+# | num2: float            |
+# | sumar() -> float       |     
+# | restar() -> float      |      
+# | multiplicar() -> float |           
+# | dividir() -> float     |
+# +------------------------+
+
+# Deberíamos incluir operación y mostrar:
+# +------------------------+
+# | Calculadora            |
+# +------------------------+
+# | num1: float            |
+# | num2: float            |
+# | operacion: string      |
+# | sumar() -> float       |     
+# | restar() -> float      |      
+# | multiplicar() -> float |           
+# | dividir() -> float     |
+# | mostrar() -> str       |
+# +------------------------+
+
+# class Calculadora:
+#     def __init__(self, n1, n2):
+#         self.num1 = float(n1)
+#         self.num2 = float(n2)
+#         self.operacion = ""
+        
+#     def sumar(self):
+#         return self.num1 + self.num2
+
+        
+#     def restar(self):
+#         return self.num1 - self.num2
+
+        
+#     def multiplicar(self):
+#         return self.num1 * self.num2
+
+        
+#     def dividir(self):
+#         return self.num1 / self.num2
+
+    
+#     def mostrar(self):
+#         if self.operacion not in ('+', '-', '*', '/'):
+#             raise Exception("Operacion debe ser +, -, * o '/'")
+        
+#         if self.operacion == "+":
+#             print(f"La suma de {self.num1} y {self.num2} es {self.sumar()}")
+#         elif self.operacion == '-':
+#             print(f"La resta de {self.num1} y {self.num2} es {self.sumar()}")
+#         elif self.operacion == '*':
+#             print(f"La multiplicacion de {self.num1} y {self.num2} es {self.sumar()}")
+#         else:
+#             print(f"La división de {self.num1} y {self.num2} es {self.sumar()}")
+
+# No es la mejor solución de hecho los prints son iguales cambiando suma, resta, multiplicación y división, eso está pidiendo a gritos una estructura de datos y dejarlo todo en un solo print. No es fácil ni banal pero te lo dejo para que le des un pensao
+# y también está pidiendo a gritos un método operar(signo) que una las dos líneas c.operacion = signo + c.mostrar()
+
 
 def inputNaturalOCero(pregunta="Introduce un número entero mayor o igual a cero: "):
     num = input(pregunta)
