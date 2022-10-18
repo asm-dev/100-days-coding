3fdf8e6b3b9ba77fc81daa88b4e4a9db0401b22a
asm-dev <salamanca.m@outlook.com>
2022-10-18 19:56:42 +0200

    Day 22

---
+++ 004-Rock paper scissors OPP/play.py
@@ -0,0 +1,64 @@
+
+from enum import Enum
+
+class Result(Enum):
+    EQUAL = 0
+    WINS = 1
+    LOSES = 2 
+
+class Play(object):
+    """
+    Represents a play
+    """
+    def compare(self, otherPlay):
+        """
+        It compares itself with the other play and returns a Result
+        """
+        pass
+
+class Paper(Play):
+    def compare(self, otherPlay):
+        """
+        Compares paper agains other plays and returns a Result
+        """
+        result = None
+        if type(otherPlay) == Paper:
+            result = Result.EQUAL
+        elif type(otherPlay) == Scissors:
+            result = Result.LOSES
+        else:
+            result = Result.WINS
+        return result
+
+class Scissors(Play):
+    def compare(self, otherPlay):
+        """
+        Compares scissor agains other plays and returns a Result
+        """
+        result = None
+        if type(otherPlay) == Paper:
+            result = Result.WINS
+        elif type(otherPlay) == Scissors:
+            result = Result.EQUAL
+        else:
+            result = Result.LOSES
+        return result
+
+class Rock(Play):
+    def compare(self, otherPlay):
+        """
+        Compares scissor agains other plays and returns a Result
+        """
+        result = None
+        if type(otherPlay) == Paper:
+            result = Result.LOSES
+        elif type(otherPlay) == Scissors:
+            result = Result.WINS
+        else:
+            result = Result.EQUAL
+        return result
+
+r = Rock()
+p = Paper()
+s = Scissors()
+print(s.compare(p))
